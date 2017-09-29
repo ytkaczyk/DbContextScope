cb38a3a22a950cbf18b8635e47f872ba3b21bef2
Tim Calvert <timcalvert@ptcllc.com>
2017-09-29 14:19:15 -0400

    Convert to EF Core.
    The original projects were replaced.

---
+++ DbContextScope/Interfaces/IDbContextCollection.cs
@@ -0,0 +1,21 @@
+﻿/* 
+ * Copyright (C) 2014 Mehdi El Gueddari
+ * http://mehdi.me
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DbContextScope {
+    /// <summary>
+    /// Maintains a list of lazily-created DbContext instances.
+    /// </summary>
+    public interface IDbContextCollection : IDisposable {
+        /// <summary>
+        /// Get or create a DbContext instance of the specified type. 
+        /// </summary>
+		TDbContext Get<TDbContext>() where TDbContext : DbContext;
+    }
+}
---
+++ Demo/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Numero3.EntityFramework.Demo.DomainModel;
+using System;
+using System.Threading.Tasks;
+
+namespace Numero3.EntityFramework.Demo.Repositories {
+    public interface IUserRepository {
+        User Get(Guid userId);
+        Task<User> GetAsync(Guid userId);
+        void Add(User user);
+    }
+}
